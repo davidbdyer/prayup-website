6fcb58dd7f7fafc68e40ed9e9b802d68c038c992
davidbdeath <social_davidbdyer@icloud.com>
2022-05-19 21:46:49 +0000

    partner page formatting

---
+++ src/assets/scripts/nav.js
@@ -0,0 +1,15 @@
+const donate = document.querySelector('#nav');
+let scrollPos = 0;
+
+window.addEventListener('scroll', () => {
+	let windowY = window.scrollY;
+
+	if (windowY < scrollPos) {
+		donate.classList.contains('nav-active');
+		donate.classList.remove('nav-active');
+	} else if (windowY > scrollPos && windowY >= 25) {
+		!donate.classList.contains('nav-active');
+		donate.classList.add('nav-active');
+	}
+	scrollPos = windowY;
+});
