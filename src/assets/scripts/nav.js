64cd2f59af0862db42295855626765de6a3815b0
davidbdeath <social_davidbdyer@icloud.com>
2022-07-01 20:30:15 +0000

    bug fixes

--- src/assets/scripts/nav.js
+++ src/assets/scripts/nav.js
@@ -1,13 +1,15 @@
 const donate = document.querySelector('#nav');
+const windowHeight = window.innerHeight;
+const bodyHeight = document.body.offsetHeight;
 let scrollPos = 0;
 
 window.addEventListener('scroll', () => {
-	let windowY = window.scrollY;
+	const windowY = window.scrollY;
 
-	if (windowY < scrollPos - 80) {
+	if ((windowY < scrollPos || windowY === 0) && windowY + windowHeight < bodyHeight) {
 		donate.classList.contains('nav-active');
 		donate.classList.remove('nav-active');
-	} else if (windowY > scrollPos && windowY >= 25) {
+	} else if (windowY + windowHeight > windowHeight) {
 		!donate.classList.contains('nav-active');
 		donate.classList.add('nav-active');
 	}
